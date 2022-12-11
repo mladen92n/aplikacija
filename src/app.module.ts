78177478b6587adba817099f217d46d81d2074f2
Novaković Mladen <mladen.novakovic.92@gmail.com>
2022-12-11 21:17:07 +0100

    Generate all remaining database entity models

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConfiguration } from 'config/database.configuratio';
 import { Administrator } from 'entities/administrator.entity';
 import { AdministratorService } from './services/administrator/administrator.service';
+import { ArticleFeature } from 'entities/article-feature.entity';
+import { Article } from 'entities/аrticle.entity';
+import { ArticlePrice } from 'entities/article-price.entity';
+import { CartArticle } from 'entities/cart-article.entity';
+import { Cart } from 'entities/cart.entity';
+import { Category } from 'entities/category.entity';
+import { Feature } from 'entities/feature.entity';
+import { Order } from 'entities/order.entity';
+import { Photo } from 'entities/photo.entity';
+import { User } from 'entities/user.entity';
 
 @Module({
   imports: [
@@ -14,7 +24,19 @@ import { AdministratorService } from './services/administrator/administrator.ser
       username: DatabaseConfiguration.username,
       password: DatabaseConfiguration.password,
       database: DatabaseConfiguration.database,
-      entities: [ Administrator ]
+      entities: [ 
+        Administrator,
+        ArticleFeature,
+        Article,
+        ArticlePrice,
+        CartArticle,
+        Cart,
+        Category,
+        Feature,
+        Order,
+        Photo,
+        User,
+      ]
     }),
     TypeOrmModule.forFeature([ Administrator ])
   ],
